092f1cf00088f3e6b7fe4b4fccf23b11979584eb
recepsen <recepsen@taptoweb.com>
2023-08-28 16:55:23 +0300

    feat: Add comments

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import "fmt"
 
+// Item struct represents a product with its name, price, and discount.
 type Item struct {
 	Name     string
 	Price    float64
 	Discount float64
 }
 
+// Describable interface defines a method for providing a description of an item.
 type Describable interface {
 	Description() string
 }
 
+// Description calculates and returns the description of an item.
 func (item Item) Description() string {
 	result := fmt.Sprintf("%s - %.2f TL", item.Name, item.Price)
 	if item.Discount > 0 {
@@ -20,13 +23,17 @@ func (item Item) Description() string {
 	return result
 }
 
+// CalculatePrice calculates the discounted price of an item.
 func CalculatePrice(item Item) float64 {
 	return item.Price - item.Discount
 }
+
+// CalculateDiscount calculates the discount percentage of an item.
 func CalculateDiscount(item Item) float64 {
-	return item.Discount / item.Price * 100
+	return (item.Discount / item.Price) * 100
 }
 
+// TotalPrice calculates the total price of a list of items.
 func TotalPrice(items []Item) (total float64) {
 	for _, item := range items {
 		total += CalculatePrice(item)
@@ -34,22 +41,24 @@ func TotalPrice(items []Item) (total float64) {
 	return
 }
 
+// PrintItem prints the description of a Describable item.
 func PrintItem(item Describable) {
 	fmt.Printf("%Q\n", item)
 }
 
+// PrintItems prints the description of a list of items and the total price.
 func PrintItems(items []Item) {
 	for _, item := range items {
 		PrintItem(item)
 	}
 	totalPrice := TotalPrice(items)
 	fmt.Printf("Toplam Fiyat: %.2f", totalPrice)
-
 }
 
+// Format formats the item based on the provided verb.
 func (item Item) Format(f fmt.State, verb rune) {
-	var value any = item
-	if verb == 81 { // check if Q passed
+	var value interface{} = item
+	if verb == 81 { // 'Q' verb is used for description
 		value = item.Description()
 	}
 	_, err := fmt.Fprint(f, value)
@@ -63,6 +72,7 @@ var elma = Item{
 	Price:    0.75,
 	Discount: 0.07,
 }
+
 var portakal = Item{
 	Name:     "Portakal",
 	Price:    0.75,
